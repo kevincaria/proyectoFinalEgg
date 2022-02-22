f1a2bebbcc9f30aa9f13e3d119159fecd075ec38
Kevin Caria <kevindcaria@gmail.com>
2022-02-21 21:09:07 -0300

    prueba

--- src/main/java/com/grupo6/app/entidades/Categoria.java
+++ src/main/java/com/grupo6/app/entidades/Categoria.java
@@ -3,9 +3,16 @@ package com.grupo6.app.entidades;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Getter
 @Setter
